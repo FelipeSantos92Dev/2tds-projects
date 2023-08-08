61645c9b9102a306b7a164d6c67b6bfc3c3d60d5
Felipe Santos <dev.felipesantos@gmail.com>
2023-08-08 16:30:19 -0300

    feat: create remove function

--- assets/js/script.posts.js
+++ assets/js/script.posts.js
@@ -50,6 +50,7 @@ function storePost(title, category, resume, author, date) {
 }
 
 function showPosts() {
+  document.getElementById("list").classList.remove("hidden");
   let showContent = "";
 
   posts.forEach((post, index) => {
@@ -62,7 +63,7 @@ function showPosts() {
         <p><strong>Data de publicação: </strong>${post.date}</p>
       
         <button onclick="editPost(${index})">Editar</button>
-        <button onclick="deletePost(${index})">Excluir</button>
+        <button onclick="removePost(${index})">Excluir</button>
       </div>
     `;
   });
@@ -80,4 +81,13 @@ function editPost(index) {
   document.getElementById("date").value = post.date;
 
   postIndex = index;
+}
+
+function removePost(index) {
+  posts.splice(index, 1);
+  showPosts();
+
+  if (posts.length == 0) {
+    document.getElementById("list").classList.add("hidden");
+  }
 }
