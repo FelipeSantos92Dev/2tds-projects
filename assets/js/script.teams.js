98dd508d24b6b4e66bfb47de42f62527bd637e4d
Felipe Santos <dev.felipesantos@gmail.com>
2023-09-06 11:24:59 -0300

    feat: create and test initial function

---
+++ assets/js/script.teams.js
@@ -0,0 +1,9 @@
+//console.log("script.teams.js loaded");
+
+function criarEquipe() {
+  const nomeDaEquipe = document.getElementById("equipe").value;
+  const quantidadeDeIntegrantes = document.getElementById("quantidade").value;
+
+  console.log("Nome da equipe: ", nomeDaEquipe);
+  console.log("Quantidade de integrantes: ", quantidadeDeIntegrantes);
+}
