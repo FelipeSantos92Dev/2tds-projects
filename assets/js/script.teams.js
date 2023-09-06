c453aa747abfdc1afdf4db18e4534f878c7604da
Felipe Santos <dev.felipesantos@gmail.com>
2023-09-06 15:49:59 -0300

    feat: create read functions

--- assets/js/script.teams.js
+++ assets/js/script.teams.js
@@ -56,8 +56,62 @@ function criarEquipe() {
 
   equipeService.adicionarEquipe(novaEquipe);
 
+  listarEquipes();
+
   //console.log("Nova equipe: ", novaEquipe);
 
   //console.log("Nome da equipe: ", nomeDaEquipe);
   //console.log("Quantidade de integrantes: ", quantidadeDeIntegrantes);
+}
+
+function listarEquipes() {
+  const listaDeEquipes = equipeService.listarEquipes();
+
+  const elementoListaDeEquipes = document.getElementById("lista-de-equipes");
+  elementoListaDeEquipes.innerHTML = "";
+
+  let content = "";
+
+  listaDeEquipes.forEach((equipe) => {
+    content += `
+    <div onclick="listarEquipeUnica(${equipe.id})">
+      
+      <p>
+        Nome: ${equipe.nome}
+      </p>
+      
+    </div>
+    `;
+  });
+
+  elementoListaDeEquipes.innerHTML = content;
+}
+
+function listarEquipeUnica(id) {
+  const equipe = equipeService.listarEquipePorId(id);
+
+  const elementoEquipe = document.getElementById("equipe-unica");
+  elementoEquipe.innerHTML = "";
+
+  let content = `
+    <div>
+      <p>
+        ID: ${equipe.id}
+      </p>
+      <p>
+        Nome: ${equipe.nome}
+      </p>
+      <p>
+        Total de jogadores: ${equipe.totalJogadores}
+      </p>
+      <p>
+        Jogadores titulares: ${equipe.titulares}
+      </p>
+      <p>
+        Jogadores reservas: ${equipe.reservas}
+      </p>
+    </div>
+    `;
+
+  elementoEquipe.innerHTML = content;
 }
